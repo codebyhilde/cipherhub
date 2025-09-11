23690d39dbcb98110fbb253dde25b42874a033cd
Hildemar Gonzalez <hildemargonzalez08@gmail.com>
2025-09-11 18:09:45 -0400

    chore(infra): add VigenereView as new route
    - Register '/vigenere' path in App routing
    - Connect existing VigenereView for cipher execution

--- refactor-vite-react/src/App.tsx
+++ refactor-vite-react/src/App.tsx
@@ -1,5 +1,6 @@
 import HomeView from "./views/HomeView.tsx";
 import CaesarView from "./views/CaesarView.tsx";
+import VigenereView from "./views/VigenereView.tsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./styles/index.css";
 
@@ -9,6 +10,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<HomeView />} />
                 <Route path="/caesar" element={<CaesarView />} />
+                <Route path="/vigenere" element={<VigenereView />} />
             </Routes>
         </BrowserRouter>
     );
